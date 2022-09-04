deed85a99ca49f04e6c2b22ca2146ff6e00d98fc
aagamdoshi1100 <108725133+aagamdoshi1100@users.noreply.github.com>
2022-09-05 00:30:09 +0530

    feat : added server processing

--- App.js
+++ App.js
@@ -1,23 +1,36 @@
-
 var TextInputArea = document.querySelector("#txt-area");
 var Buttn = document.querySelector("#Cl");
 var OutputArea = document.querySelector("#Output");
 
-OutputArea.innerText = "Helloworld";
+// OutputArea.innerText = "Helloworld";
+// console.log(Buttn)
+// console.log(TextInputArea)
 
-console.log(Buttn)
-console.log(TextInputArea)
+var URL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 
+function ConnectionURL(text1){  
 
+    return finalUrl = URL + "?" + "text=" + text1;
+}
+
+function errorHandler(error){
+    console.log("error occured",error)
+}
 
 function ClickHandler(){
-    console.log(Buttn);
-    console.log("input", TextInputArea.value);
 
-    var abc = TextInputArea.value;
+    var text = TextInputArea.innerText;
 
-OutputArea.innerText = abc;
+    fetch(ConnectionURL(text))
+    .then(response => response.json())
+    .then(json => {
+        var TranslatedText = json.contents.translated;
+        OutputArea.innerText =  TranslatedText;
+    })
+    .catch(errorHandler)
+    // console.log(Buttn);
+    // console.log("input", TextInputArea.value);
+    // var abc = TextInputArea.value;
+    // OutputArea.innerText = abc;
 };
-
-
 Buttn.addEventListener("click", ClickHandler);
