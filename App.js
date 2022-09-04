ba1e9859263c98dc7180d0a28d95e2b3736b0923
aagamdoshi1100 <108725133+aagamdoshi1100@users.noreply.github.com>
2022-09-04 14:41:59 +0530

    feat : I/O processing

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 
 var TextInputArea = document.querySelector("#txt-area");
 var Buttn = document.querySelector("#Cl");
+var OutputArea = document.querySelector("#Output");
+
+OutputArea.innerText = "Helloworld";
+
 console.log(Buttn)
 console.log(TextInputArea)
+
+
 
 function ClickHandler(){
     console.log(Buttn);
     console.log("input", TextInputArea.value);
+
+    var abc = TextInputArea.value;
+
+OutputArea.innerText = abc;
 };
 
 
